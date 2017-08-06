b45fa582b91a2f9046685e42e6616ce080bc19d4
tjeckleburg <tjeckleburg@users.noreply.github.com>
2017-08-06 14:37:09 -0700

    Anagram solution study

--- 2.Analysis/2.4.1.AnagramSolution.py
+++ 2.Analysis/2.4.1.AnagramSolution.py
@@ -1,56 +1,82 @@
 #! /opt/local/bin/python3
 
+
 def isAnagramV1(s1,s2):
 
+    """ Return True if s1 and s2 are anagrams 
+
+    s1, s2: Strings of equal length.
+    Book solution with O(n2) run time
+    """
+    alist = list(s2)
+
+    pos1 = 0
+    stillOK = True
+
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+
+        pos1 = pos1 + 1
+
+    return stillOK
+
+
+def isAnagramV2(s1,s2):
     """ Return False if s2 is not an anagram of s1 
 
     s1, s2: Strings of equal length.
 
-    Start by assuming s2 is an anagram of s1.
-    Select one character in s1, check each character of s2
-    If there is a match found, proceed with another character in s1
+    Form a dictionary {character: count} for both strings
+    If the two dictionary=ries are equal, then the two strings are anagrams.
 
-    If this test fails at any character, then s2 is not and anagram of s1. Return False
+    """
+    
+    d1 = {}
+    for c in s1:
+        if c in d1:
+            d1[c] += 1
+        else:
+            d1[c] = 1
 
-    If this test never fails, then s2 really is anagram of s1. Return True
+    d2 = {}
+    for c in s2:
+        if c in d2:
+            d2[c] += 1
+        else:
+            d2[c] = 1
 
-
-    """ 
-
-    for c1 in s1:
-        checkNextInS1 = False
-        for c2 in s2:
-            if c2 == c1:
-                checkNextInS1 = True
-                break
-        if not checkNextInS1:
-            return False
-            break
-
-    return True
-
-
-
-
+    return d1 == d2
 
 
 def main():
 
     import timeit
 
-    s1 = "python"
+    s1 = "p"
+    s2 = "p"
 
-    s2 = "pyhton"
 
-    print(isAnagramV1(s1,s2))
+    #print(isAnagramV1(s1,s2))
+    #print(isAnagramV2(s1,s2))
 
-    # for n in range(10):
-    #     s1 = s1 + s1
-    #     s2 = s1
-    #     t1 = timeit.Timer(("isAnagramV1('%s','%s')" % (s1,s2)),"from __main__ import isAnagramV1")
-    #     print(("Number of characters: %10d, time it took: %10e") % (len(s1),t1.timeit(1)))
+    for n in range(15):
+        s1 = s1 + s1
+        s2 = s2 + s2
+        t1 = timeit.Timer(("isAnagramV1('p'+'%s'+'p','p'+'%s'+'q')" % (s1,s2)),"from __main__ import isAnagramV1")
+        t2 = timeit.Timer(("isAnagramV2('p'+'%s'+'p','p'+'%s'+'q')" % (s1,s2)),"from __main__ import isAnagramV2")
+        print(("Number of characters: %10d, time it took with v1: %10e, time it took with v2: %10e") % (len(s1),t1.timeit(1), t2.timeit(1)))
 
- 
 
 if __name__ == "__main__":
     main()
