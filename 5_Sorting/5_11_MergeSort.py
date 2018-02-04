b581158b75ddc9888064faba1dec1736f3095c29
tjeckleburg <tjeck1925@gmail.com>
2018-02-04 14:13:31 -0800

    Added a take on in-place mergesort.

--- 5_Sorting/5_11_MergeSort.py
+++ 5_Sorting/5_11_MergeSort.py
@@ -44,9 +44,12 @@ def mergeSort2(L):
     #nr=n-np2
     #Define split points
     bL=0
-    eL=6
+    eL=n//2+1
+    print("eL:",eL)
     bR=eL
     eR=n
+    """
+    #HDA out: This will create extra O(n) memory.
     #Split into L and Right sublists
     sL=L[bL:eL]
     sR=L[bR:eR]
@@ -62,27 +65,76 @@ def mergeSort2(L):
     while i<dL and j<dR:
         #print("i={},j={},k={}".format(i,j,k))
         if sL[i]<=sR[j]:
-           L[k]=sL[i]
-           i+=1
+            L[k]=sL[i]
+            i+=1
         else:
-           L[k]=sR[j]
-           j+=1
+            L[k]=sR[j]
+            j+=1
         k+=1
     #print("done while 1")
     while i<dL:
-       L[k]=sL[i]
-       i+=1
-       k+=1
+        L[k]=sL[i]
+        i+=1
+        k+=1
     #print("done while 2")
     while i<dR:
-       L[k]=sR[j]
-       j+=1
-       k+=1
+        L[k]=sR[j]
+        j+=1
+        k+=1
     #print("done while 3")
+    """
+ 
+    #HDA in: This should do it in-place:
+    dL=eL-bR-1
+    dR=eR-bR-1
+
+    i=bL
+    j=bR
+    k=bL
+
+    while i<eL and j<eR:
+        print("currently:",L)
+        print("i={},j={},k={}".format(i,j,k))
+        if  L[i]<=L[j]:
+            L[k]=L[i]
+            i+=1
+            j+=1
+        else:
+            L[k],L[j]=L[j],L[k]
+            i+=1
+        k+=1
+    #print("done while 1")
+
+    while i<dL:
+        L[k]=L[i]
+        i+=1
+        k+=1
+    #print("done while 2")
+
+    while i<dR:
+        L[k]=L[j]
+        j+=1
+        k+=1
+    #print("done while 3")
+   
+   print"(This is gonna be more complicated than I thought. Leaving aside for now." 
+   #HDA done.
+
+
+   #After figureing out how to merge two sorted arrays in place, do multiple passes.
+   #Each pass k should work on sub-arrays of 2**k size:
+   #For example, for a list of 19 elements:
+   #Pass 0: [0]   , [1]   , [2]   , [3]    , [4]   , [5]    , [6]    , [7]    , [8] , [9], [10], [11], [12], [13], [14], [15], [16], [17], [19]
+   #Pass 1: [0:2] , [2:4] , [4:6 ], [6:8]  , [8:10], [10:12], [12:14], [14:16], [16:]
+   #Pass 2: [0:4] , [4:8] , [8:12], [12:16], [16:]
+   #Pass 3: [0:8] , [8:16], [16:]
+   #Pass 4: [0:16], [16:]
+ 
 
 def test_mergeSort():
     """Test if mergeSort works as expected."""
-    for mergeSort in [mergeSort1, mergeSort2]:
+    #for mergeSort in [mergeSort1, mergeSort2]:
+    for mergeSort in [mergeSort2]:
         print("Testing: {:s}".format(mergeSort.__name__))
         #Reinitialize the test inputs:
         a_sorted_list           =[0,1,2,3,4,5,6,7,8,9,10]
@@ -115,8 +167,9 @@ if __name__ == "__main__":
     import bigO
 
     test_mergeSort()
-
+    """
     for mergeSort in [mergeSort1, mergeSort2]:
         print("Benchmarking: {}".format(mergeSort.__name__))
         bigO.analyze(mergeSort)
+    """
 
